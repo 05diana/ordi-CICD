37f102d7f074619cccdaf7a1c740f1ab56c1055b
05diana <111016421+05diana@users.noreply.github.com>
2025-01-17 15:33:27 -0500

    Create ecs_service/variable.tf
    modules/ecs_service

---
+++ modules/ecs_service/variable.tf
@@ -0,0 +1,15 @@
+
+variable "vpc_id" {}
+variable "db_user" {}
+variable "db_name" {}
+variable "db_pass" {}
+variable "db_host" {}
+variable "vpc_cidr" {}
+variable "db_sq_id" {}
+variable "domain_name" {}
+variable "cluster_arn" {}
+variable "micro_service" {}
+variable "registry_image" {}
+variable "public_subnet_ids" {}
+variable "private_subnet_ids" {}
+variable "ssl_certificate_arn" {}
